3abf3983b60b02554f19fe9616086ae9428ad1ae
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-07 14:19:11 -0300

    feat: include provider in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import { Box, CssBaseline, ThemeProvider } from '@mui/material'
 import { GlobalProvider } from 'contexts/GlobalContext'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import MainRoutes from 'routes'
+import store from 'store/config'
 import GlobalStyles from 'theme/globalStyles'
 import theme from 'theme/index'
 
@@ -13,19 +15,22 @@ root.render(
     <BrowserRouter>
       <CssBaseline />
       <GlobalStyles />
-      <GlobalProvider>
-        <ThemeProvider theme={theme}>
-          <Box
-            bgcolor={theme.palette.background.default}
-            display='flex'
-            justifyContent='center'
-            minHeight='100vh'
-            width='100%'
-          >
-            <MainRoutes />
-          </Box>
-        </ThemeProvider>
-      </GlobalProvider>
+      <Provider store={store}>
+        {/* TODO: remove this providers after configure redux-store */}
+        <GlobalProvider>
+          <ThemeProvider theme={theme}>
+            <Box
+              bgcolor={theme.palette.background.default}
+              display='flex'
+              justifyContent='center'
+              minHeight='100vh'
+              width='100%'
+            >
+              <MainRoutes />
+            </Box>
+          </ThemeProvider>
+        </GlobalProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
