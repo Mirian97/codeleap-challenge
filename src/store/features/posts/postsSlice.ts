64c7bee53baf91a862af8bb0c37642b463b5dbad
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-07 19:20:54 -0300

    feat: createAsyncThunk to get posts

--- src/store/features/posts/postsSlice.ts
+++ src/store/features/posts/postsSlice.ts
@@ -1,17 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { getPosts as getPostsApi } from 'services/post'
+import { POSTS_INITIAL_STATE } from './constants'
 
-export const postsSlice = createSlice({
-  name: 'post',
-  initialState: {},
-  reducers: {
-    getPosts: (state, action) => {},
-    addPost: (state, action) => {},
-    editPost: (state, action) => {},
-    deletePost: (state, action) => {}
+export const getPostsThunk = createAsyncThunk(
+  'posts/getPosts',
+  async (_, { rejectWithValue }) => {
+    try {
+      return await getPostsApi()
+    } catch (error) {
+      return rejectWithValue(error)
+    }
+  }
+)
+
+export const postSlice = createSlice({
+  name: 'posts',
+  initialState: POSTS_INITIAL_STATE,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPostsThunk.pending, (state) => {
+        state.status = 'isLoading'
+      })
+      .addCase(getPostsThunk.fulfilled, (state, action) => {
+        console.log(action.payload)
+        state.status = 'idle'
+        state.posts = action.payload.results
+      })
+      .addCase(getPostsThunk.rejected, (state, action) => {
+        state.status = 'error'
+        state.error = action.payload as string
+      })
   }
 })
 
-//here where I create action creators
-export const { getPosts, addPost, editPost, deletePost } = postsSlice.actions
-
-export default postsSlice.reducer
+export default postSlice.reducer
