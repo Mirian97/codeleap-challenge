0c9158d8a17fbafea24c91edfb35452660863312
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-07 21:47:20 -0300

    feat: createAsyncThunks to add, edit and delete post

--- src/store/features/posts/postsSlice.ts
+++ src/store/features/posts/postsSlice.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { getPosts as getPostsApi } from 'services/post'
+import { createPost, deletePost, editPost, getPosts as getPostsApi } from 'services/post'
+import { TPost, TPostWithoutId } from 'types/post'
 import { POSTS_INITIAL_STATE } from './constants'
 
 export const getPostsThunk = createAsyncThunk(
@@ -9,6 +10,48 @@ export const getPostsThunk = createAsyncThunk(
       return await getPostsApi()
     } catch (error) {
       return rejectWithValue(error)
+    }
+  }
+)
+
+export const createPostThunk = createAsyncThunk(
+  'posts/createPost',
+  async (payload: Omit<TPost, 'id'>, { rejectWithValue, dispatch }) => {
+    try {
+      await createPost(payload)
+      return dispatch(getPostsThunk())
+    } catch (error) {
+      rejectWithValue(error)
+    }
+  }
+)
+
+export const editPostThunk = createAsyncThunk(
+  'posts/editPost',
+  async (
+    payload: {
+      id: number
+      body: TPostWithoutId
+    },
+    { rejectWithValue, dispatch }
+  ) => {
+    try {
+      await editPost(payload)
+      return dispatch(getPostsThunk())
+    } catch (error) {
+      rejectWithValue(error)
+    }
+  }
+)
+
+export const deletePostThunk = createAsyncThunk(
+  'posts/deletPost',
+  async (payload: { id: number }, { rejectWithValue, dispatch }) => {
+    try {
+      await deletePost(payload)
+      return dispatch(getPostsThunk())
+    } catch (error) {
+      rejectWithValue(error)
     }
   }
 )
@@ -27,6 +70,37 @@ export const postSlice = createSlice({
         state.posts = action.payload.results
       })
       .addCase(getPostsThunk.rejected, (state, action) => {
+        state.status = 'error'
+        //TODO: handle better with this error
+        state.error = action.payload as string
+      })
+      .addCase(createPostThunk.pending, (state) => {
+        state.status = 'isLoading'
+      })
+      .addCase(createPostThunk.fulfilled, (state) => {
+        state.status = 'idle'
+      })
+      .addCase(createPostThunk.rejected, (state, action) => {
+        state.status = 'error'
+        state.error = action.payload as string
+      })
+      .addCase(editPostThunk.pending, (state) => {
+        state.status = 'isLoading'
+      })
+      .addCase(editPostThunk.fulfilled, (state) => {
+        state.status = 'idle'
+      })
+      .addCase(editPostThunk.rejected, (state, action) => {
+        state.status = 'error'
+        state.error = action.payload as string
+      })
+      .addCase(deletePostThunk.pending, (state) => {
+        state.status = 'isLoading'
+      })
+      .addCase(deletePostThunk.fulfilled, (state) => {
+        state.status = 'idle'
+      })
+      .addCase(deletePostThunk.rejected, (state, action) => {
         state.status = 'error'
         state.error = action.payload as string
       })
