46e2bc7cd9cd031cd8b58719924a16226254effe
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-13 17:53:42 -0300

    fix: currentPostSlice to return the state correctly

--- src/store/features/currentPost/currentPostSlice.ts
+++ src/store/features/currentPost/currentPostSlice.ts
@@ -1,10 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { TPost } from 'types/post'
 import { CURRENT_POST_INITIAL_STATE } from './constants'
 
 export const currentPostSlice = createSlice({
   name: 'currentPost',
-  initialState: CURRENT_POST_INITIAL_STATE,
-  reducers: {}
+  initialState: CURRENT_POST_INITIAL_STATE as TPost,
+  reducers: {
+    setCurrentPost: (_, { payload }: PayloadAction<TPost>) => {
+      return payload
+    }
+  }
 })
 
+export const { setCurrentPost } = currentPostSlice.actions
 export default currentPostSlice.reducer
