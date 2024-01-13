af9479b81c4ecc2efa1e9c01e27bd6abc6b26777
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-13 15:29:25 -0300

    feat: create local storage utils

---
+++ src/utils/storage.ts
@@ -0,0 +1,17 @@
+export const setItem = (key: string, value: string | object) => {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+export const getItem = (key: string) => {
+  const storage = localStorage.getItem(key)
+  if (storage === null) return
+  return JSON.parse(storage)
+}
+
+export const clearItem = (key: string) => {
+  localStorage.removeItem(key)
+}
+
+export const clearAll = () => {
+  localStorage.clear()
+}
